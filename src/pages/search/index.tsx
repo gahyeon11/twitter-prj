2ccada7977a0ba617e83ef110abad55d683a146f
gahyeon <keh5667@gmail.com>
2025-06-18 17:29:12 +0900

    Feat: 검색 구현

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,68 @@
+import PostBox from "components/posts/PostBox";
+import AuthContext from "context/AuthContext";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import { useContext, useEffect, useState } from "react";
+
 export default function SearchPage() {
-  return <h1>Post List Page</h1>;
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [tagQuery, setTagQuery] = useState<string>("");
+  const { user } = useContext(AuthContext);
+
+  const onChange = (e: any) => {
+    setTagQuery(e.target.value.trim());
+  };
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postsQuery = query(
+        postsRef,
+        where("hashTags", "array-contains-any", [tagQuery]),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot?.docs?.map((doc) => ({
+          ...doc?.data(),
+          id: doc?.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
+
+  return (
+    <div className="home">
+      <div className="home_top">
+        <div className="home_title">
+          <div className="home_title-text">Search</div>
+        </div>
+        <div className="home_search-div">
+          <input
+            className="home_search"
+            placeholder="해시태그를 입력해주세요."
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <div className="post">
+        {posts?.length > 0 ? (
+          posts?.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post_no-posts">
+            <div className="post_text">게시글이 없습니다.</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
